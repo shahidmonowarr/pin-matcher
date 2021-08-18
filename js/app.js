f3bf5544e8e1b1a55a9648402ebab87d62852986
shahidmonowarr <shahidmonowar21@gmail.com>
2021-08-19 00:27:27 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+function generatePin() {
+    const pin = getPin()
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const calcInput = document.getElementById('typed-numbers');
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+})
